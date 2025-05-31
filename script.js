8df73c0e54d71bdd299e8d5757c54b3ce85a616c
prem prakash <jyotiprakashjkp@gmail.com>
2025-05-31 10:56:53 +0530

    idea
    new changes idea

---
+++ script.js
@@ -0,0 +1,21 @@
+
+  const filterButtons = document.querySelectorAll('.filter-btn');
+  const portfolioItems = document.querySelectorAll('.portfolio-item');
+
+  filterButtons.forEach(btn => {
+    btn.addEventListener('click', () => {
+      document.querySelector('.filter-btn.active').classList.remove('active');
+      btn.classList.add('active');
+
+      const filter = btn.getAttribute('data-filter');
+
+      portfolioItems.forEach(item => {
+        if (filter === 'all' || item.classList.contains(filter)) {
+          item.style.display = 'block';
+        } else {
+          item.style.display = 'none';
+        }
+      });
+    });
+  });
+
